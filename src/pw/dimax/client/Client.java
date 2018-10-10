77d12d875ad76689ce09de242b113b0f8110551b
ValRus69 <bbll@mail.ru>
2018-10-10 17:28:08 +0300

    004

--- src/pw/dimax/client/Client.java
+++ src/pw/dimax/client/Client.java
@@ -1,84 +1,43 @@
 package pw.dimax.client;
 
+
 import java.io.*;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.*;
 
 public class Client {
+    public static void main(String[] args) throws IOException {
 
+        System.out.println("Welcome to Client side");
 
-    public static void main(String[] args) throws InterruptedException {
+        Socket fromserver = null;
 
-// запускаем подключение сокета по известным координатам и нициализируем приём сообщений с консоли клиента
-        try(Socket socket = new Socket("localhost", 3345);
-            BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-            DataOutputStream oos = new DataOutputStream(socket.getOutputStream());
-            DataInputStream ois = new DataInputStream(socket.getInputStream()); )
-        {
+        //орпаав
 
-            System.out.println("Client connected to socket.");
-            System.out.println();
-            System.out.println("Client writing channel = oos & reading channel = ois initialized.");
+        System.out.println("Connecting to localhost");
 
-// проверяем живой ли канал и работаем если живой
-            while(!socket.isOutputShutdown()){
+        fromserver = new Socket("localhost",4444);
+        BufferedReader in  = new
+                BufferedReader(new
+                InputStreamReader(fromserver.getInputStream()));
+        PrintWriter    out = new
+                PrintWriter(fromserver.getOutputStream(),true);
+        BufferedReader inu = new
+                BufferedReader(new InputStreamReader(System.in));
 
-// ждём консоли клиента на предмет появления в ней данных
-                if(br.ready()){
+        String fuser,fserver;
 
-// данные появились - работаем
-                    System.out.println("Client start writing in channel...");
-                    Thread.sleep(1000);
-                    String clientCommand = br.readLine();
-
-// пишем данные с консоли в канал сокета для сервера
-                    oos.writeUTF(clientCommand);
-                    oos.flush();
-                    System.out.println("Clien sent message " + clientCommand + " to server.");
-                    Thread.sleep(1000);
-// ждём чтобы сервер успел прочесть сообщение из сокета и ответить
-
-// проверяем условие выхода из соединения
-                    if(clientCommand.equalsIgnoreCase("quit")){
-
-// если условие выхода достигнуто разъединяемся
-                        System.out.println("Client kill connections");
-                        Thread.sleep(2000);
-
-// смотрим что нам ответил сервер на последок перед закрытием ресурсов
-                        if(ois.read() > -1)     {
-                            System.out.println("reading..." + ois.read());
-                            String in = ois.readUTF();
-                            System.out.println(in);
-                        }
-
-// после предварительных приготовлений выходим из цикла записи чтения
-                        break;
-                    }
-
-// если условие разъединения не достигнуто продолжаем работу
-                    System.out.println("Client sent message & start waiting for data from server...");
-                    Thread.sleep(2000);
-
-// проверяем, что нам ответит сервер на сообщение(за предоставленное ему время в паузе он должен был успеть ответить)
-                    if(ois.read() > -1)     {
-
-// если успел забираем ответ из канала сервера в сокете и сохраняем её в ois переменную,  печатаем на свою клиентскую консоль
-                        System.out.println("reading...");
-                        String in = ois.readUTF();
-                        System.out.println(in);
-                    }
-                }
-            }
-// на выходе из цикла общения закрываем свои ресурсы
-            System.out.println("Closing connections & channels on clentSide - DONE.");
-
-        } catch (UnknownHostException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        while ((fuser = inu.readLine())!=null) {
+            out.println(fuser);
+            fserver = in.readLine();
+            System.out.println(fserver);
+            if (fuser.equalsIgnoreCase("close")) break;
+            if (fuser.equalsIgnoreCase("exit")) break;
         }
+
+        out.close();
+        in.close();
+        inu.close();
+        fromserver.close();
     }
 }
+
