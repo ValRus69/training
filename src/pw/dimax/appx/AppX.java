c79899b567cb3524ed8ff627feb0141642eebc78
ValRus69 <bbll@mail.ru>
2018-10-15 21:46:05 +0300

    007

--- src/pw/dimax/appx/AppX.java
+++ src/pw/dimax/appx/AppX.java
@@ -9,19 +9,6 @@ public class AppX {
 
     public static void main(String args[]){
 
-        DataBase db = new DataBase();
-        CachedRowSet crs = db.executeQuery("select * from PERSON");
 
-        try {
-            while (crs.next()) {
-                for (int i = 0; i < crs.getMetaData().getColumnCount(); i++) {
-                    Log.print(crs.getMetaData().getColumnName(i+1) +
-                            ": " + crs.getString(i+1));
-                }
-                Log.print("===============");
-            }
-        } catch (SQLException e){
-            Log.exception(e);
-        }
     }
 }
--- src/pw/dimax/server/Server.java
+++ src/pw/dimax/server/Server.java
@@ -1,69 +1,57 @@
 package pw.dimax.server;
 
-import pw.dimax.util.Log;
 
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
 
-public class Server {
+public class Server implements Runnable{
+
+    private static final int BUFFER_SIZE = 1024;
+    private int port;
+    private InetAddress host;
+    private Selector selector;
 
     public static void main(String[] args) {
 
-        try (ServerSocket server= new ServerSocket(3345)){
-            Socket client = server.accept();
 
-            System.out.println("Connection accepted.");
+    }
 
-            DataOutputStream out = new DataOutputStream(client.getOutputStream());
-            DataInputStream in = new DataInputStream(client.getInputStream());
+    @Override
+    public void run() {
+        while(true) {
 
-            out.writeUTF("rr");
-            out.flush();
-
-            //test callback
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-            while(!client.isClosed()){
-
-                System.out.println("Server reading from channel");
-                String entry = in.readUTF();
-
-                System.out.println("READ from client message - " + entry);
-
-                if(entry.equalsIgnoreCase("quit")){
-                    System.out.println("Client initialize connections suicide ...");
-                    out.writeUTF("Server reply - "+entry + " - OK");
-                    out.flush();
-                    break;
-                }
-
-
-                if(br.ready()){
-                    String s = br.readLine();
-                    out.writeUTF(s);
-                    Log.print(s);
-                   // out.flush();
-                }
-
-                out.writeUTF("555 OK");
-                //out.flush();
-
-            }
-
-
-            System.out.println("Client disconnected");
-            System.out.println("Closing connections & channels.");
-
-            in.close();
-            out.close();
-
-            client.close();
-
-
-            System.out.println("Closing connections & channels - DONE.");
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+    }
+
+    public Server() throws IOException{
+        this(InetAddress.getLocalHost(), 7272);
+    }
+
+    public Server(InetAddress host, int port) throws IOException {
+        this.host = host;
+        this.port = port;
+    }
+
+    private Selector initSelector() throws IOException {
+
+        // Create a new selector
+        Selector socketSelector = Selector.open();
+
+        // Create a new non-blocking server socket channel
+        ServerSocketChannel socketChannel = ServerSocketChannel.open();
+        socketChannel.configureBlocking(false);
+
+        // Bind the server socket to the specified address and port
+        socketChannel.socket().bind( new InetSocketAddress(this.host, this.port) );
+
+
+
+
+
+        return null;
     }
 }
